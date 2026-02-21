d60906fec7bf5d36ee8ea43f3ff764315895a786
divyani <ikrakizoi2607@gmail.com>
2026-02-21 21:50:30 +0530

    Delete qiedex/backend/config directory

--- qiedex/backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI, {
-    dbName: "qiedex"
-  });
-  console.log("✅ MongoDB connected");
-}
